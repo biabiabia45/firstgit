0b43d9c9bce421919978b4b0e9b10b80e71f2c71
biabiabia45 <1183990820@qq.com>
2024-08-07 12:31:56 +0800

    v2.1 修改mapper，服务器异常

--- WalletOnline/src/main/java/myproject/wallet/WalletOnline.java
+++ WalletOnline/src/main/java/myproject/wallet/WalletOnline.java
@@ -1,11 +1,9 @@
 package myproject.wallet;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@MapperScan("myproject.wallet.domain.mapper")
+@SpringBootApplication  
 public class WalletOnline {
     public static void main(String[] args) {
         SpringApplication.run(WalletOnline.class, args);
